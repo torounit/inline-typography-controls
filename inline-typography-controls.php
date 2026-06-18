0242ae5aeac7ddb48246409e0e2e30b628566199
torounit <mail@torounit.com>
2026-06-19 04:45:12 +0900

    Release 0.5.0

--- inline-typography-controls.php
+++ inline-typography-controls.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Inline Typography Controls
  * Description: Add inline typography controls to the editor.
- * Version: 0.4.8
+ * Version: 0.5.0
  * Author: Toro_Unit
  * License: GPL-3.0+
  * GitHub Plugin URI: https://github.com/torounit/inline-typography-controls
